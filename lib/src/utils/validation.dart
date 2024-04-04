ec0f6f73381ae9c2f927ab0891df0cb93e848ab5
Kounex <r.schramowski@googlemail.com>
2024-04-04 15:33:34 +0200

    feat(password validation): added password validation

--- lib/src/utils/validation.dart
+++ lib/src/utils/validation.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/cupertino.dart';
 
+enum PasswordType {
+  digitLetter,
+  digitLetterSpecial,
+  digitLetterLowerUpper,
+  digitLetterLowerUpperSpecial;
+
+  RegExp regex({
+    int minLength = 8,
+    int? maxLength,
+  }) =>
+      RegExp(switch (this) {
+        PasswordType.digitLetter =>
+          '^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{$minLength,${maxLength ?? ""}}',
+        PasswordType.digitLetterSpecial =>
+          '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{$minLength,${maxLength ?? ""}',
+        PasswordType.digitLetterLowerUpper =>
+          '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{$minLength,${maxLength ?? ""}',
+        PasswordType.digitLetterLowerUpperSpecial =>
+          '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{$minLength,${maxLength ?? ""}',
+      });
+
+  String get errorMessage => switch (this) {
+        PasswordType.digitLetter =>
+          'Needs to have at least one letter and one digit!',
+        PasswordType.digitLetterSpecial =>
+          'Needs to have at least one letter, one digit and one special character!',
+        PasswordType.digitLetterLowerUpper =>
+          'Needs to have at least one lowercase letter, one uppercase letter and one digit!',
+        PasswordType.digitLetterLowerUpperSpecial =>
+          'Needs to have at least one lowercase letter, one uppercase letter, one digit and one special character!',
+      };
+}
+
 class ValidationUtils {
   static bool _empty(String? text) => text == null || text.trim().isEmpty;
 
@@ -12,6 +45,28 @@ class ValidationUtils {
     }
     if (name!.trim().length < minLength) {
       return 'At least $minLength characters!';
+    }
+    return null;
+  }
+
+  static String? password(String? password, PasswordType type,
+      {bool required = true, int minLength = 8, int? maxLength}) {
+    if (_empty(password)) {
+      if (required) {
+        return 'Field is required!';
+      }
+      return null;
+    }
+    if (password!.trim().length < minLength) {
+      return 'At least $minLength characters!';
+    }
+    if (maxLength != null && password.trim().length > maxLength) {
+      return 'At least $minLength characters!';
+    }
+
+    if (!password
+        .contains(type.regex(minLength: minLength, maxLength: maxLength))) {
+      return type.errorMessage;
     }
     return null;
   }
