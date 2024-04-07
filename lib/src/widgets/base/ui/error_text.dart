681a56d4cb0162a1025a903e9fc42ff23ffa7760
Kounex <r.schramowski@googlemail.com>
2024-04-07 22:53:38 +0200

    feat(errortext): added baseerrortext widget

--- lib/src/widgets/base/ui/error_text.dart
+++ lib/src/widgets/base/ui/error_text.dart
@@ -9,23 +9,34 @@ class BaseErrorText extends StatelessWidget {
   /// and if [true] we will use [Theme.of(context).colorScheme.error]
   final bool useThemeError;
 
+  final bool dense;
+  final TextStyle? style;
+
   const BaseErrorText(
     this.error, {
     super.key,
     this.center = true,
     this.useThemeError = false,
+    this.dense = false,
+    this.style,
   });
 
   @override
   Widget build(BuildContext context) {
+    TextStyle style = (this.style ??
+            (this.dense
+                ? Theme.of(context).textTheme.labelMedium!
+                : const TextStyle()))
+        .copyWith(
+      color: this.useThemeError
+          ? Theme.of(context).colorScheme.error
+          : CupertinoColors.destructiveRed,
+    );
+
     return Text(
       this.error,
       textAlign: this.center ? TextAlign.center : null,
-      style: TextStyle(
-        color: this.useThemeError
-            ? Theme.of(context).colorScheme.error
-            : CupertinoColors.destructiveRed,
-      ),
+      style: style,
     );
   }
 }
