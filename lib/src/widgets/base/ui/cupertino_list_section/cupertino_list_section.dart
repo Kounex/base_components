8c50808ca4de57791a522f7a2026efab2e87c394
Kounex <r.schramowski@googlemail.com>
2025-04-22 18:00:49 +0200

    new cupertino list section

--- lib/src/widgets/base/ui/cupertino_list_section/cupertino_list_section.dart
+++ lib/src/widgets/base/ui/cupertino_list_section/cupertino_list_section.dart
@@ -1,4 +1,3 @@
-import 'package:base_components/src/utils/design_system.dart';
 import 'package:base_components/src/widgets/base/ui/cupertino_list_section/cupertino_list_tile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,9 +21,7 @@ class BaseCupertinoListSection extends StatelessWidget {
     return CupertinoListSection.insetGrouped(
       backgroundColor: Colors.transparent,
       margin: EdgeInsets.zero,
-      dividerMargin: this.hasLeading
-          ? this.dividerMargin ?? DesignSystem.spacing.x24
-          : 14.0,
+      dividerMargin: this.hasLeading ? this.dividerMargin ?? 22.0 : 14.0,
       hasLeading: this.hasLeading,
       children: this.customChildren ?? this.tiles,
     );
